c18f2bd0950716a475f44739e4a09561dbda5630
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2019-07-22 19:11:11 +1000

    Refactored testing code.

--- RolePlayingGame/TestsProject/UnitTest1.cs
+++ RolePlayingGame/TestsProject/UnitTest1.cs
@@ -1,15 +1,26 @@
 using NUnit.Framework;
+using RolePlayingGame;
 
 namespace Tests {
     public class Tests {
+
 
         [SetUp]
         public void Setup() {
         }
 
         [Test]
-        public void Test1() {
-            Assert.Pass();
+        public void PlayerTakesDamage() {
+            var player = new Player(10);
+            player.TakeDamage(1);
+            Assert.AreEqual(9, player.currentHealth);
+        }
+
+        [Test]
+        public void PlayerHealsDamage() {
+            var player2 = new Player(9, 10);
+            player2.Heal(1);
+            Assert.AreEqual(10, player2.currentHealth);
         }
     }
 }
