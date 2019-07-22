5a9587151908703a3765ad0619ba0aa1d7fbca5e
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2019-07-22 19:57:55 +1000

    Enemy class

---
+++ RolePlayingGame/RolePlayingGame/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RolePlayingGame.Interfaces;
+
+namespace RolePlayingGame
+{
+    class Enemy : IActor
+    {
+        private int _currentHealth;
+        private int _maximumHealth;
+
+        public Enemy(int currentHealth, int maxHealth) {
+            _currentHealth = currentHealth;
+            _maximumHealth = maxHealth;
+        }
+
+        public Enemy(int maxHealth) {
+            _currentHealth = maxHealth;
+            _maximumHealth = maxHealth;
+        }
+
+        public int currentHealth {
+            get { return _currentHealth; }
+            set { _currentHealth = value; }
+        }
+
+        public int maxHealth {
+            get { return _maximumHealth; }
+            set { _maximumHealth = value; }
+        }
+
+        public void Heal(int healing) {
+            if ((currentHealth + healing) >= maxHealth) {
+                currentHealth = maxHealth;
+            }
+            else {
+                currentHealth += healing;
+            }
+        }
+
+        public void TakeDamage(int damage) {
+            if ((currentHealth - damage) <= 0) {
+                currentHealth = 0;
+            }
+            else {
+                currentHealth -= damage;
+            }
+        }
+    }
+}
