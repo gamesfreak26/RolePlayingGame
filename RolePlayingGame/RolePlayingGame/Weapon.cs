47ddacf454bbf0130210ebfc2db3c1e587a58690
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2019-07-23 21:42:50 +1000

    Added a Weapon class

---
+++ RolePlayingGame/RolePlayingGame/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RolePlayingGame.Interfaces;
+
+namespace RolePlayingGame
+{
+    class Weapon : IItem
+    {
+        public Weapon(string description, int proficiencyBonus) {
+            Description = description;
+            ProficiencyBonus = proficiencyBonus;
+        }
+        
+        List<string> types = new List<string> {
+            "None",
+            "Sword",
+            "Axe"
+        };
+
+        public string Description { get; private set; }
+        public int ProficiencyBonus { get; private set; }
+    }
+}
